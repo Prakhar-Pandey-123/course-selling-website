4d860ecef62f8f253605ba70c10b15c8b969dc6b
Prakhar Pandey <prakhar9704@gmail.com>
2025-04-17 19:36:00 +0530

    initial commit

---
+++ server/models/Profile.js
@@ -0,0 +1,16 @@
+//rsme->require,schema,model,exports
+const mongoose=require("mongoose");
+
+const profileSchema= new mongoose.Schema({
+    gender:{
+        type:String
+    },
+    dateOfBirth:{
+        type:String
+    },
+    contactNumber:{
+        type:Number,// now this is something new
+        trim:true
+    }
+});
+module.exports=mongoose.model("Profile",profileSchema);
