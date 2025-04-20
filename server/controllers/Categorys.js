1106e153b6075bd4353e57b0a07501e137f37834
Prakhar Pandey <prakhar9704@gmail.com>
2025-04-20 23:26:58 +0530

    added controllers for rating and review

--- server/controllers/Categorys.js
+++ server/controllers/Categorys.js
@@ -5,8 +5,8 @@ exports.createCategory = async (req, res) => {
         try {//fetch data ,to be send by the admin
                 const { name, description } = req.body;
                 //validation
-                if (!name || !description ) {
-                        return res .status(400).json({ success: false, message: "All fields are required" });
+                if (!name || !description) {
+                        return res.status(400).json({ success: false, message: "All fields are required" });
                 }
                 //create entyr in db
                 const CategorysDetails = await Category.create({
@@ -29,29 +29,42 @@ exports.createCategory = async (req, res) => {
 exports.showAllCategories = async (req, res) => {
         try {
                 const allCategorys = await Category.find({},
-                    { name: true, description: true } );
-//{} =a filter. Empty {} means “get all documents”,
-// It means “only return name and description fields”.
+                        { name: true, description: true });
+                //{} =a filter. Empty {} means “get all documents”,
+                // It means “only return name and description fields”.
                 res.status(200).json({
                         success: true,
-                        data: allCategorys, });
-                    }
-         catch (error) {
+                        data: allCategorys,
+                });
+        }
+        catch (error) {
                 return res.status(500).json({
                         success: false,
                         message: error.message,
                 });
         }
 };
-
+//to show all courses related to a category searched by user + show some popular courses too + show courses other than that category
 exports.categoryPageDetails = async (req, res) => {
         try {
                 const { categoryId } = req.body;
-
                 // Get courses for the specified category
-                const selectedCategory = await Category.findById(categoryId)          //populate instuctor and rating and reviews from courses
-                        .populate({path:"courses",match:{status:"Published"},populate:([{path:"instructor"},{path:"ratingAndReviews"}])})
+                const selectedCategory = await Category.findById(categoryId)
+                        .populate({
+                                path: "courses",
+                                match: { status: "Published" },
+                                populate: [
+                                        { path: "instructor" },
+                                        { path: "ratingAndReviews" } 
+                                ]
+                        })
                         .exec();
+                       // You use , to separate multiple populate objects inside an array.
+                         // populate the 'courses' field in Category
+        // only include courses with status 'Published'(apply filter)
+                        // further populate fields inside each course
+                        // populate the 'instructor' field of the course
+// populate the 'ratingAndReviews' field of the course
                 // console.log(selectedCategory);
                 // Handle the case when the category is not found
                 if (!selectedCategory) {
@@ -73,15 +86,15 @@ exports.categoryPageDetails = async (req, res) => {
 
                 // Get courses for other categories
                 const categoriesExceptSelected = await Category.find({
-                        _id: { $ne: categoryId },
-                }).populate({path:"courses",match:{status:"Published"},populate:([{path:"instructor"},{path:"ratingAndReviews"}])});
+                        _id: { $ne: categoryId },//ne=not equals
+                }).populate({ path: "courses", match: { status: "Published" }, populate: ([{ path: "instructor" }, { path: "ratingAndReviews" }]) });
                 let differentCourses = [];
                 for (const category of categoriesExceptSelected) {
                         differentCourses.push(...category.courses);
                 }
 
                 // Get top-selling courses across all categories
-                const allCategories = await Category.find().populate({path:"courses",match:{status:"Published"},populate:([{path:"instructor"},{path:"ratingAndReviews"}])});
+                const allCategories = await Category.find().populate({ path: "courses", match: { status: "Published" }, populate: ([{ path: "instructor" }, { path: "ratingAndReviews" }]) });
                 const allCourses = allCategories.flatMap((category) => category.courses);
                 const mostSellingCourses = allCourses
                         .sort((a, b) => b.sold - a.sold)
@@ -121,7 +134,7 @@ exports.addCourseToCategory = async (req, res) => {
                                 message: "Course not found",
                         });
                 }
-                if(category.courses.includes(courseId)){
+                if (category.courses.includes(courseId)) {
                         return res.status(200).json({
                                 success: true,
                                 message: "Course already exists in the category",
